2e518b950da80c918b1cbbe033fa2133af640914
0xrickjames <0xrickjames@gmail.com>
2023-09-29 20:13:11 -0400

    first commit

---
+++ theme.config.js
@@ -0,0 +1,76 @@
+export default {
+  github: 'https://github.com/shuding/nextra',
+  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
+  titleSuffix: ' – Lands of Ascension',
+  logo: (
+    <>
+      <span className="mr-2 font-extrabold hidden md:inline">
+        Lands of Ascension RPG Bot
+      </span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta
+        name="description"
+        content="Lands of Ascension RPG Bot is a Discord bot that allows you to battle friends and bosses to earn gold and experience. Buy wepons, armor, spells, and skills to aid you in battle!"
+      />
+      <meta
+        name="og:description"
+        content="Lands of Ascension RPG Bot is a Discord bot that allows you to battle friends and bosses to earn gold and experience. Buy wepons, armor, spells, and skills to aid you in battle!"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:image"
+        content="https://cdn.discordapp.com/attachments/939309405227339776/1157077674280353822/LoA_logo.png"
+      />
+      <meta name="twitter:site:domain" content="nextra.vercel.app" />
+      <meta name="twitter:url" content="https://nextra.vercel.app" />
+      <meta name="og:title" content="Lands of Ascension RPG Bot" />
+      <meta
+        name="og:image"
+        content="https://cdn.discordapp.com/attachments/939309405227339776/1157077674280353822/LoA_logo.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="OctoMob" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+    </>
+  ),
+  search: true,
+  prevLinks: true,
+  nextLinks: true,
+  footer: false,
+  unstable_faviconGlyph: '🐙',
+}
